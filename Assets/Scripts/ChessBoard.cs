1dc4b2102ba6cd7af3ab64c349771b99f5fbe11b
Huynh Tuan Duy <74585439+tuanduy18012002@users.noreply.github.com>
2022-11-11 20:05:46 +0700

    Update Change color pointed Cell

--- Assets/Scripts/ChessBoard.cs
+++ Assets/Scripts/ChessBoard.cs
@@ -9,16 +9,44 @@ public class ChessBoard : MonoBehaviour
     public int size_col;
 
     Cell[][] m_cells;
+
+    private RaycastHit version;
+    float rayLength;
+    Camera currentCamera;
+    (int, int) index;
+
     // Start is called before the first frame update
     void Start()
     {
         DisplayChessBoard(size_row, size_col);
+        rayLength = 100;
+        index = (-1, -1);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+         if (!currentCamera)
+        {
+            currentCamera = Camera.current;
+            return;
+        }
+
+        Ray ray = currentCamera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out version, rayLength))
+        {
+            if (version.collider.tag == "Cell")
+            {
+                ResetPre_Cell_color();
+
+                index = LookupCell(version.transform.gameObject);
+                if (index.Item1 != -1 && index.Item2 != -1)
+                {
+                    m_cells[index.Item1][index.Item2].ChangeColor(2);
+
+                }
+            }
+        }
     }
 
     void DisplayChessBoard(int n_row, int n_col)
@@ -48,5 +76,36 @@ public class ChessBoard : MonoBehaviour
     {
         Vector3 spawnPos = new Vector3(posX, posY, posZ);
         return Instantiate(cell, spawnPos, Quaternion.identity) as GameObject;
+    }
+
+    (int, int) LookupCell(GameObject hitVer)
+    {
+        Cell temp = hitVer.GetComponent<Cell>();
+        for (int i = 0; i < size_row; i++)
+        {
+            for (int j = 0; j < size_col; j++)
+            {
+                if (m_cells[i][j] == temp)
+                {
+                    return (i, j);
+                }
+            }
+        }
+        return (-1, -1);
+    }
+
+        void ResetPre_Cell_color()
+    {
+        if (index != (-1,-1))
+        {
+            if ((index.Item1 + index.Item2) % 2 == 0)
+            {
+                m_cells[index.Item1][index.Item2].ChangeColor(0);
+            }
+            else
+            {
+                m_cells[index.Item1][index.Item2].ChangeColor(1);
+            }
+        }
     }
 }
